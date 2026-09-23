88b84e809a0184e4573e52887d6ccb93bbb525bc
K00lman <meepsterIQ@yahoo.com>
2026-09-23 01:00:02 -0700

    Started work on the class itself, included constructors and added SI namespace

--- UnitIO.cpp
+++ UnitIO.cpp
@@ -1,1 +1,20 @@
 #include "UnitIO.h"
+
+Smart_Unit_Container::Smart_Unit_Container(const double value, const units::precise_unit unit) {
+    input_value = units::precise_measurement(value, unit);
+    // Checks the default unit map to see if we have a predefined base unit for the inputted unit
+    if (const auto base_units = Default_Units.find(unit.base_units()); base_units != Default_Units.end()) {
+        output_value = units::precise_measurement(units::convert(value, unit, base_units->second), base_units->second);
+    } else {
+        // If not, simply use the .base_units() value
+        auto fallback_base_units = units::precise_unit(unit.base_units());
+        output_value =
+                units::precise_measurement(units::convert(value, unit, fallback_base_units), fallback_base_units);
+    }
+}
+
+Smart_Unit_Container::Smart_Unit_Container(const double value, units::precise_unit input_unit,
+                                           units::precise_unit output_unit) {
+    input_value = units::precise_measurement(value, input_unit);
+    output_value = units::precise_measurement(units::convert(value, input_unit, output_unit), output_unit);
+}
--- UnitIO.h
+++ UnitIO.h
@@ -2,18 +2,58 @@
 #define UNITIO_LIBRARY_H
 
 #include <llnl-units/units.hpp>
+#include <unordered_map>
 
 #include "UnitIO.h"
 
 namespace UnitIO {
     // Unit systems
     namespace Units = units::precise; // All units
+    namespace SI {
+        inline constexpr auto &meter = Units::meter;
+        inline constexpr auto &kilogram = Units::kilogram;
+        inline constexpr auto &second = Units::second;
+        inline constexpr auto &amp = Units::Ampere;
+        inline constexpr auto &kelvin = Units::Kelvin;
+        inline constexpr auto &mole = Units::mol;
+        inline constexpr auto &candela = Units::candela;
+        inline constexpr auto &radians = Units::radian;
+        inline constexpr auto &hertz = Units::hertz;
+        inline constexpr auto &volt = Units::volt;
+        inline constexpr auto &pascal = Units::Pa;
+        inline constexpr auto &newton = Units::newton;
+        inline constexpr auto &joule = Units::joule;
+        inline constexpr auto &watt = Units::watt;
+        inline constexpr auto &coulomb = Units::coulomb;
+        inline constexpr auto &lumen = Units::lumen;
+        inline constexpr auto &milligram = Units::mg;
+        inline constexpr auto &gram = Units::g;
+        inline constexpr auto &milliliter = Units::mL;
+        inline constexpr auto &liter = Units::L;
+        inline constexpr auto &nanometer = Units::nm;
+        inline constexpr auto &millimeter = Units::mm;
+        inline constexpr auto &kilometer = Units::km;
+        inline constexpr auto &centimeter = Units::cm;
+        inline constexpr auto &bar = Units::bar;
+    };
     namespace Customary = Units::us; // US customary system
 
     // Commonly used units
-    inline constexpr auto& meter = Units::meter;
+    inline constexpr auto &meter = Units::meter;
 }
 
-class Smart_Unit_Container {};
+inline static const std::unordered_map<units::detail::unit_data, units::precise_unit> Default_Units = {};
+
+class Smart_Unit_Container {
+    units::precise_measurement input_value;
+    units::precise_measurement output_value;
+
+public:
+    Smart_Unit_Container(double value, units::precise_unit unit);
+
+    Smart_Unit_Container(double value, units::precise_unit input_unit, units::precise_unit output_unit);
+
+    ~Smart_Unit_Container() = default;
+};
 
 #endif // UNITIO_LIBRARY_H
